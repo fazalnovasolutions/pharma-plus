f210bc66190feed203124c155d747dbf0ef61fdd
Fazal <fazalkhann66@gmail.com>
2021-09-25 15:14:08 +0500

    base - first commit

---
+++ resources/views/layouts/leftbar.blade.php
@@ -0,0 +1,51 @@
+<div class="leftbar">
+    <!-- Start Sidebar -->
+    <div class="sidebar">
+        <!-- Start Logobar -->
+        <div class="logobar">
+            <a href="{{url('/')}}" class="logo logo-large"><img src="assets/images/logo.svg" class="img-fluid" alt="logo"></a>
+            <a href="{{url('/')}}" class="logo logo-small"><img src="assets/images/small_logo.svg" class="img-fluid" alt="logo"></a>
+        </div>
+        <!-- End Logobar -->
+        <!-- Start Navigationbar -->
+        <div class="navigationbar">
+            <ul class="vertical-menu">
+                <li>
+                    <a href="javaScript:void();">
+                      <img src="assets/images/svg-icon/pages.svg" class="img-fluid" alt="pages"><span>Pages</span><i class="feather icon-chevron-right pull-right"></i>
+                    </a>
+                    <ul class="vertical-submenu">
+                        <li>
+                            <a href="javaScript:void();">Basic<i class="feather icon-chevron-right pull-right"></i></a>
+                            <ul class="vertical-submenu">
+                                <li><a href="{{url('/page-starter')}}">Starter</a></li>
+                                <li><a href="{{url('/page-blog')}}">Blog</a></li>
+                                <li><a href="{{url('/page-faq')}}">FAQ</a></li>
+                                <li><a href="{{url('/page-gallery')}}">Gallery</a></li>
+                                <li><a href="{{url('/page-invoice')}}">Invoice</a></li>
+                                <li><a href="{{url('/page-pricing')}}">Pricing</a></li>
+                                <li><a href="{{url('/page-timeline')}}">Timeline</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="javaScript:void();">Authentications<i class="feather icon-chevron-right pull-right"></i></a>
+                            <ul class="vertical-submenu">
+                                <li><a href="{{url('/user-login')}}">Login</a></li>
+                                <li><a href="{{url('/user-register')}}">Register</a></li>
+                                <li><a href="{{url('/user-forgotpsw')}}">Forgot Password</a></li>
+                                <li><a href="{{url('/user-lock-screen')}}">Lock Screen</a></li>
+                                <li><a href="{{url('/error-comingsoon')}}">Coming Soon</a></li>
+                                <li><a href="{{url('/error-maintenance')}}">Maintenance</a></li>
+                                <li><a href="{{url('/error-404')}}">Error 404</a></li>
+                                <li><a href="{{url('/error-500')}}">Error 500</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+
+            </ul>
+        </div>
+        <!-- End Navigationbar -->
+    </div>
+    <!-- End Sidebar -->
+</div>
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="description" content="Orbiter is a bootstrap minimal & clean admin template">
+        <meta name="keywords" content="admin, admin panel, admin template, admin dashboard, responsive, bootstrap 4, ui kits, ecommerce, web app, crm, cms, html, sass support, scss">
+        <meta name="author" content="Themesbox">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title> @yield('title') </title>
+        <!-- Fevicon -->
+        <link rel="shortcut icon" href="{{ asset('assets/images/favicon.ico') }}">
+        <!-- Start CSS -->   
+        @yield('style')
+        <link href="{{ asset('assets/plugins/switchery/switchery.min.css') }}" rel="stylesheet" type="text/css">
+        <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css">
+        <link href="{{ asset('assets/css/icons.css') }}" rel="stylesheet" type="text/css">
+        <link href="{{ asset('assets/css/flag-icon.min.css') }}" rel="stylesheet" type="text/css">
+        <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet" type="text/css">
+        <!-- End CSS -->
+    </head>
+    <body class="vertical-layout">
+        <!-- Start Containerbar -->
+        <div id="containerbar">     
+            <!-- Start Leftbar -->
+            @include('layouts.leftbar')
+            <!-- End Leftbar -->
+            <!-- Start Rightbar -->
+            @include('layouts.rightbar')          
+            @yield('content')
+            <!-- End Rightbar --> 
+        </div>
+        <!-- End Containerbar -->
+        <!-- Start JS -->        
+        <script src="{{ asset('assets/js/jquery.min.js') }}"></script>
+        <script src="{{ asset('assets/js/popper.min.js') }}"></script>
+        <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
+        <script src="{{ asset('assets/js/modernizr.min.js') }}"></script>
+        <script src="{{ asset('assets/js/detect.js') }}"></script>
+        <script src="{{ asset('assets/js/jquery.slimscroll.js') }}"></script>
+        <script src="{{ asset('assets/js/vertical-menu.js') }}"></script> 
+        <script src="{{ asset('assets/plugins/switchery/switchery.min.js') }}"></script> 
+        @yield('script')
+        <!-- Core JS -->
+        <script src="{{ asset('assets/js/core.js') }}"></script>
+        <!-- End JS -->
+    </body>
+</html>    
